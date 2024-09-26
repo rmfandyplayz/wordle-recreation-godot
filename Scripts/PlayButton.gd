6ef8d5b4cfc562069b0243ecc920defbb4b68465
rmfandyplayz <andyjia2500@gmail.com>
2024-09-26 00:13:31 -0700

    Update 1

--- Scripts/PlayButton.gd
+++ Scripts/PlayButton.gd
@@ -1,19 +1,68 @@
 extends Button
 
-@export var tweenDuration : float
-@export var tweenIntensity : float
+#this script handles the animation for:
+#- hover over the play button
+#- un-hovering the play button
+#- pressing the play button
 
-var button = self
+#buttons that don't mention "which button" are assumed to be the button this script is attached to (play button)
+@export var tweenDuration : float
+@export var pressedTweenDuration : float
+@export var buttonHoverSizeModifier : float
+@onready var originalButtonSize : Vector2 = size
+
+@onready var settingsButton : Button = $"../Settings"
+@onready var settingsButtonPos : Vector2 = settingsButton.position
+
+@onready var jumpscareButton : Button = $"../Mr Kinney Jumpscare"
+@onready var jumpscareButtonPos : Vector2 = jumpscareButton.position
+
+var isPlayButtonActive : bool = false #if the play button is pressed and in the play menu, don't allow the mouse enter and exit things happen
+
+
+
+
+func _on_pressed() -> void:
+	var tween = create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	isPlayButtonActive = true
+	
+	tween.tween_property(self, "scale", Vector2.ONE, pressedTweenDuration)
+	tween.tween_property(self, "scale", Vector2.ONE * buttonHoverSizeModifier, tweenDuration)
+	
+	await tween.finished
+	$"../Settings".disabled = true
+	$"../Mr Kinney Jumpscare".disabled = true
+	
+	#animation for the buttons under the play submenu goes under here
+	#note: the size for the buttons post-play should be 156 pixels in height.
+	
+	#fade out the text and icon
+	#continue here
+	
+	
 
 func _on_mouse_entered() -> void:
-	button.pivot_offset = button.size / 2
-	#button.
-	var tween = create_tween().set_trans(Tween.TRANS_CIRC)
+	var tween = create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	
+	if isPlayButtonActive == false:
+		tween.tween_property(self, "scale", Vector2.ONE * buttonHoverSizeModifier, tweenDuration)
+		tween.parallel().tween_property($"../Settings", "position", settingsButtonPos + Vector2(0, 15), tweenDuration)
+		tween.parallel().tween_property($"../Mr Kinney Jumpscare", "position", jumpscareButtonPos + Vector2(0, 15), tweenDuration)
 
-	tween.tween_property(self, "size", Vector2.ONE * tweenIntensity, tweenDuration)
 
 func _on_mouse_exited() -> void:
-	button.pivot_offset = button.size / 2
-	var tween = create_tween().set_trans(Tween.TRANS_CUBIC)
+	var tween = create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 	
-	tween.tween_property(self, "scale", Vector2.ONE, tweenDuration)
+	if isPlayButtonActive == false:
+		tween.tween_property(self, "scale", Vector2.ONE, tweenDuration)
+		tween.parallel().tween_property($"../Settings", "position", settingsButtonPos, tweenDuration)
+		tween.parallel().tween_property($"../Mr Kinney Jumpscare", "position", jumpscareButtonPos, tweenDuration)
+
+
+#this should be called after pressing the back button after pressing the play button
+#this function restores the original buttons' properties
+func RestoreButtons():
+	isPlayButtonActive = false
+	$"../Settings".disabled = false
+	$"../Mr Kinney Jumpscare".disabled = false
+	emit_signal("mouse_exited")
