791407c02c6f7588acc5cc7aba4f2944ca5faefb
rmfandyplayz <andyjia2500@gmail.com>
2024-10-07 17:54:26 -0700

    Update 11.1

--- Scripts/MainMenu/CustomizeCloseButton.gd
+++ Scripts/MainMenu/CustomizeCloseButton.gd
@@ -2,6 +2,7 @@ extends Button
 
 
 func _on_pressed() -> void:
+	disabled = true
 	var tween = create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
 	tween.tween_property($"..", "scale", Vector2(0, 0), 0.25)
 	
