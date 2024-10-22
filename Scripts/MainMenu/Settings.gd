43ad77295edac75bfdb1a7a9f275aff74aab1acc
rmfandyplayz <andyjia2500@gmail.com>
2024-10-21 19:53:54 -0700

    Update 24

--- Scripts/MainMenu/Settings.gd
+++ Scripts/MainMenu/Settings.gd
@@ -1,1 +1,15 @@
 extends Node
+
+
+func _on_master_volume_drag_ended(value_changed: bool) -> void:
+	pass # Replace with function body.
+
+
+
+func _on_music_volume_drag_ended(value_changed: bool) -> void:
+	pass # Replace with function body.
+
+
+
+func _on_sfx_volume_drag_ended(value_changed: bool) -> void:
+	pass # Replace with function body.
